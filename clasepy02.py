9122620f2637c6cadffb98fbd7cc71d24d328f1d
Mephyphel <89886422+Mephyphel@users.noreply.github.com>
2021-09-28 20:23:32 -0500

    Create clasepy02.py

---
+++ clasepy02.py
@@ -0,0 +1,27 @@
+
+print(type("1"))
+print("1")
+
+print(type("hola mundo"))
+print("hola mundo")
+
+print(type(1))
+print(1)
+
+carro = 5
+numero = "carro"
+texto = "1"
+numero_direccion = 524
+piso_casa=1
+
+print(type(numero))
+print(numero)
+
+print(type (texto))
+print(texto)
+
+print("la suma es :", numero+numero)
+
+print("El reporte del formulario es  :", numero_direccion+piso_casa)
+factura={"pan","huevos", 100, 1234}
+print(factura)
